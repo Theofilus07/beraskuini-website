96430bb9dc12f12e03d8ed647c72f20b6adb3312
Theofilus Slamet <94041506+Theofilus07@users.noreply.github.com>
2026-03-01 18:34:11 +0800

    refactor: Use static asset paths for images
    Replaces dynamic image imports with static paths in Footer, Navbar, and product data. This simplifies asset loading and improves performance by allowing the build tool to better optimize image assets.

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,61 +1,52 @@
-import lahapAnak from "/beras-lahap-anak.png";
-import lahapSp from "/beras-lahap-sp.png";
-import anugerah from "/beras-anugerah.png";
-import beras38 from "/beras-38.png";
-import duaJempol from "/beras-2-jempol.png";
-import mawarMerah from "/beras-mawar-merah.png";
-import mutiara from "/beras-mutiara.png";
-import santap from "/beras-santap.png";
-
 export const products = [
   {
     id: 1,
     name: "Beras Lahap Anak",
     price_25kg: 393000,
-    image: lahapAnak,
+    image: "/beras-lahap-anak.png",
     is_featured: true,
   },
   {
     id: 2,
     name: "Beras Lahap SP",
     price_25kg: 393000,
-    image: lahapSp,
+    image: "/beras-lahap-sp.png",
     is_featured: true,
   },
   {
     id: 3,
     name: "Beras Anugerah",
     price_25kg: 362000,
-    image: anugerah,
+    image: "/beras-anugerah.png",
   },
   {
     id: 4,
     name: "Beras 38",
     price_25kg: 383000,
-    image: beras38,
+    image: "/beras-38.png",
   },
   {
     id: 5,
     name: "Beras 2 Jempol",
     price_25kg: 362000,
-    image: duaJempol,
+    image: "/beras-2-jempol.png",
   },
   {
     id: 6,
     name: "Beras Mawar Merah",
     price_25kg: 358000,
-    image: mawarMerah,
+    image: "/beras-mawar-merah.png",
   },
   {
     id: 7,
     name: "Beras Mutiara",
     price_25kg: 387000,
-    image: mutiara,
+    image: "/beras-mutiara.png",
   },
   {
     id: 8,
     name: "Beras Santap",
     price_25kg: 393000,
-    image: santap,
+    image: "/beras-santap.png",
   }
 ];
